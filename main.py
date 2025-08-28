d71f724a2ef4ddf9a7262d00b64a3f9c93871483
deltak96-alt <deltak96@gmail.com>
2025-08-28 21:21:10 +0630

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name_
+
+@app.route("/")
+
+def home():
+
+return "Hello, Render! Your bot is working
+
+_name ==" main ":
+
+app.run(host="0.0.0.0", port=5000)
